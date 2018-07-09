de3a4600674ff1ce6cbea58d151bbc434c9dd185
chejianchao <820992376@qq.com>
2018-07-09 03:26:52 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -7,13 +7,14 @@ def load_library(path)
   emotion["get_emoticon"] = {}
   emotion["get_meaning"] = {}
   emotion.each{|meaning, icons|
-    emotion["get_emoticon"][icons[1]] = meaning
-    emotion["get_meaning"][icons[0]] = icons[1]
+    emotion["get_meaning"][icons[1]] = meaning
+    emotion["get_emoticon"][icons[0]] = icons[1]
   }
 end
 
-def get_japanese_emoticon
+def get_japanese_emoticon(path)
   # code goes here
+  emotion = load_library(path)
   
 end
 
